7324e8be49ff3408c4c9e27153bbdb409c258df4
Shaury Baranwal <shaury29.baranwal@gmail.com>
2017-10-15 19:17:40 +0530

    update app.js

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-Shaury Chutiya
+var request=require('request');
+var cheerio=require('cheerio');
+var fs=require('fs');
+var urls=[];
+
+request('http://www.delhi.gov.in/wps/wcm/connect/DoIT_Health/health/home/',function(err,res,body){
+	if(!err && res.statusCode==200){
+		var $=cheerio.load(body);
+		$('a','#tab-cont1').each(function(){
+			var url=$(this).text();
+			urls.push(url);
+		});
+	}
+	console.log(urls);
+	
+	for(var i=0;i<urls.length;i++){
+		//first create a directory img inside current directory
+		request(urls[i]).pipe(fs.createWriteStream('img/'+i+'.jpg'));
+	} 
+});
